5769df2fd886e9d5deb94ec665db67a9b66f0e4f
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-06-12 18:35:50 +0200

    country, not name

--- src/AddPlayer.svelte
+++ src/AddPlayer.svelte
@@ -12,14 +12,14 @@
     e.preventDefault();
     dispatch("addplayer", player);
     player = {
-      name: "",
+      country: "",
       points: 0
     };
   };
 </script>
 
 <form on:submit={onSubmit} class="grid-3">
-  <input type="text" placeholder="Player Name" bind:value={player.name} />
+  <input type="text" placeholder="Player Name" bind:value={player.country} />
   <input type="number" placeholder="Player Points" bind:value={player.points} />
   <input type="submit" class="btn btn-primary" value="Add Player" />
 </form>
