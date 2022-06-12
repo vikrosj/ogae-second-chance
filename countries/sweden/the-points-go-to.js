43de1726722703fbc1ecfd3bcf69e5019b6ca0a9
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-06-12 18:32:58 +0200

    a few starting points

--- countries/sweden/the-points-go-to.js
+++
@@ -1,3 +0,0 @@
-export const points = {
-    'Norway' : 12
-}
---
+++ voting/points.js
@@ -0,0 +1,6 @@
+export default
+[
+    {id:1, country:'Norway', points: {'Sweden' : 1, 'Denmark' : 12}},
+    {id:2, country:'Sweden', points: {'Norway' : 1, 'Denmark' : 12}},
+    {id:3, country:'Denmark', points: {'Sweden' : 1, 'Norway' : 12}}
+   ];
