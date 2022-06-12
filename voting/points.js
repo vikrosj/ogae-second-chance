d1503dcf511cc5e9f347f8f074f1357ca7cc7f60
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-06-12 19:48:13 +0200

    add country codes for flags

--- voting/points.js
+++ voting/points.js
@@ -1,6 +1,6 @@
 export default
 [
-    {id:1, country:'Norway', points: {'Sweden' : 1, 'Denmark' : 12}},
-    {id:2, country:'Sweden', points: {'Norway' : 1, 'Denmark' : 12}},
-    {id:3, country:'Denmark', points: {'Sweden' : 1, 'Norway' : 12}}
-   ];
+    {id:1, country:'Norway', country_code: 'NO', points: {'Sweden' : 1, 'Denmark' : 12}},
+    {id:2, country:'Sweden', country_code: 'SE', points: {'Norway' : 1, 'Denmark' : 12}},
+    {id:3, country:'Denmark', country_code: 'DK', points: {'Sweden' : 1, 'Norway' : 12}}
+   ]
