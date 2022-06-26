0caa875db5f41fabfa7cfa6c6cbfceb5ca76f3c1
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-06-26 21:03:52 +0200

    add all countries from 2022

--- voting/participants.js
+++ voting/participants.js
@@ -2,23 +2,163 @@ import { writable } from 'svelte/store';
 
 export let participantsArray = writable([
     {
-        "Name" : "Norway",
-        "Points" : [],
-        "CountryCode" : "NO"
+        Name : "Albania",
+        Alpha2Code: "AL"
     },
     {
-        "Name" : "Sweden",
-        "Points" : [],
-        "CountryCode": "SE"
+        Name: "Armenia",
+        Alpha2Code: "AM"
     },
     {
-        "Name" : "Denmark",
-        "Points" : [],
-        "CountryCode" : "DK"
+        Name: "Australia",
+        Alpha2Code: "AU"
     },
     {
-        "Name" : "United Kingdom",
-        "Points" : [],
-        "CountryCode" : "GB"
-    }
+        Name: "Austria",
+        Alpha2Code: "AT"
+    },
+    {
+        Name: "Azerbaijan",
+        Alpha2Code: "AZ"
+    },
+    {
+        Name: "Belgium",
+        Alpha2Code: "BE"
+    },
+    {
+        Name: "Bulgaria",
+        Alpha2Code: "BG"
+    },
+    {
+        Name: "Croatia",
+        Alpha2Code: "HR"
+    },
+    {
+        Name: "Cyprus",
+        Alpha2Code: "CY"
+    },
+    {
+        Name: "Czech Republic",
+        Alpha2Code: "CZ"
+    },
+    {
+        Name: "Denmark",
+        Alpha2Code: "DK"
+    },
+    {
+        Name: "Estonia",
+        Alpha2Code: "EE"
+    },
+    {
+        Name: "Finland",
+        Alpha2Code: "FI"
+    },
+    {
+        Name: "France",
+        Alpha2Code: "FR"
+    },
+    {
+        Name: "Germany",
+        Alpha2Code: "DE"
+    },
+    {
+        Name: "Georgia",
+        Alpha2Code: "GE"
+    },
+    {
+        Name: "Greece",
+        Alpha2Code: "GR"
+    },
+    {
+        Name: "Iceland",
+        Alpha2Code: "IS"
+    },
+    {
+        Name: "Ireland",
+        Alpha2Code: "IE"
+    },
+    {
+        Name: "Israel",
+        Alpha2Code: "IL"
+    },
+    {
+        Name: "Italy",
+        Alpha2Code: "IT"
+    },
+    {
+        Name: "Latvia",
+        Alpha2Code: "LV"
+    },
+    {
+        Name: "Lithuania",
+        Alpha2Code: "LT"
+    },
+    {
+        Name: "Malta",
+        Alpha2Code: "MT"
+    },
+    {
+        Name: "Moldova",
+        Alpha2Code: "MD"
+    },
+    {
+        Name: "Montenegro",
+        Alpha2Code: "ME"
+    },
+    {
+        Name: "The Netherlands",
+        Alpha2Code: "NL"
+    },
+    {
+        Name: "North Macedonia",
+        Alpha2Code: "MK"
+    },
+    {
+        Name: "Norway",
+        Alpha2Code: "SE"
+    },
+    {
+        Name: "Poland",
+        Alpha2Code: "PL"
+    },
+    {
+        Name: "Portugal",
+        Alpha2Code: "PT"
+    },
+    {
+        Name: "Romania",
+        Alpha2Code: "RO"
+    },
+    {
+        Name: "San Marino",
+        Alpha2Code: "SM"
+    },
+    {
+        Name: "Serbia",
+        Alpha2Code: "RS"
+    },
+    {
+        Name: "Slovenia",
+        Alpha2Code: "SI"
+    },
+    {
+        Name: "Spain",
+        Alpha2Code: "ES"
+    },
+    {
+        Name: "Sweden",
+        Alpha2Code: "SE"
+    },
+    {
+        Name: "Switzerland",
+        Alpha2Code: "CH"
+    },
+    {
+        Name: "Ukraine",
+        Alpha2Code: "UA"
+    },
+    {
+        Name: "United Kingdom",
+        Alpha2Code: "UK"
+    },
   ]);
