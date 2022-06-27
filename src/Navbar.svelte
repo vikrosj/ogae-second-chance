e163c759c2165a43bd4018bf9bd9ca1f02459d30
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-06-27 21:50:53 +0200

    add css gradient background

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,4 +1,21 @@
-<div class="navbar bg-primary">
-  <h1>test</h1>
-  <!-- <img src="./img/esc_norway.jpg" alt="logo"/> -->
+<div class="navbar-background">
+  <img src="static/esc_norway_edit.png" alt="logo"/>
 </div>
+
+<style>
+  img {
+  width: 20%;
+  height: auto;
+  margin-right: 0%;
+  margin-bottom: 0%;
+}
+
+.navbar-background {
+  background-color: #4158D0;
+  background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);
+  margin-bottom: 0%;
+  margin-top: 0%;
+}
+
+ 
+</style>
