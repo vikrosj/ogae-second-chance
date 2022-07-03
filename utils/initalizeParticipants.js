4ec4da8f63618d8924f7d171f2e2490ad4836faa
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-07-03 15:39:52 +0200

    old

--- utils/initalizeParticipants.js
+++
@@ -1,8 +0,0 @@
-
-export default function initalizeParticipants(array, country_code, name) {
-		array.forEach(el => {
-		el.value ++;
-		country_code.push(el.Alpha2Code);
-    name.push(el.Name);
-	})
-	}
