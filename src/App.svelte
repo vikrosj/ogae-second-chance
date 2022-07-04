a867cc841a4077bb9b29a03f9977bfd654f6f43c
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-07-04 11:43:41 +0200

    button design

--- src/App.svelte
+++ src/App.svelte
@@ -38,7 +38,7 @@
 </div>
 
 <div class="points-button">
-  <button on:click={() => onClick()}>Give points</button>
+  <button class="button-2" on:click={() => onClick()}>Give points</button>
 </div>
 
 
@@ -61,4 +61,30 @@
   right: 100px;
   bottom: 50px;
 }
+
+.button-2 {
+
+  display: inline-block;
+                outline: 0;
+                border:0;
+                cursor: pointer;
+                text-decoration: none;
+                position: relative;
+                color: #000;
+                background: rgb(238,174,202);;
+                line-height: 30px;
+                border-radius: 40px;
+                padding: 20px;
+                font-size: 30px;
+                font-weight: 600;
+                box-shadow: rgb(255, 198, 0) -2px -2px 0px 2px, rgb(246, 84, 174) 0px 0px 0px 4px, rgba(0, 0, 0, 0.05) 0px 0px 2px 7px;
+                transition: all 0.2s;
+                
+}
+
+.button-2:hover {
+  background-color: yellow; /* Green */
+  color: black;
+}
+                
 </style>
