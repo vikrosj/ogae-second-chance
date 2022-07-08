96b2ca184ab8e8ca03891742648860e78ccc1e1d
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-07-08 12:53:47 +0200

    similar buttons

--- src/PointsButton.svelte
+++ src/PointsButton.svelte
@@ -33,7 +33,7 @@
 </script>
 
 <div class="points-button">
-    <button class="button-2" on:click={() => onClick()}>Give points</button>
+    <button class="button-2" on:click={() => onClick()}>1-10 points</button>
 </div>
 
 <div class="points-text">
@@ -50,27 +50,27 @@
 
 .points-button {
   position: absolute;
-  right: 75px;
+  right: 300px;
   bottom: 50px;
 }
 
 .button-2 {
 
   display: inline-block;
-                outline: 0;
-                border:0;
-                cursor: pointer;
-                text-decoration: none;
-                position: relative;
-                color: #000;
-                background: rgb(238,174,202);;
-                line-height: 30px;
-                border-radius: 40px;
-                padding: 20px;
-                font-size: 30px;
-                font-weight: 600;
-                box-shadow: rgb(255, 198, 0) -2px -2px 0px 2px, rgb(246, 84, 174) 0px 0px 0px 4px, rgba(0, 0, 0, 0.05) 0px 0px 2px 7px;
-                transition: all 0.2s;
+  outline: 0;
+  border:0;
+  cursor: pointer;
+  text-decoration: none;
+  position: relative;
+  color: #000;
+  background: rgb(238,174,202);;
+  line-height: 30px;
+  border-radius: 40px;
+  padding: 20px;
+  font-size: 30px;
+  font-weight: 600;
+  box-shadow: rgb(255, 198, 0) -2px -2px 0px 2px, rgb(246, 84, 174) 0px 0px 0px 4px, rgba(0, 0, 0, 0.05) 0px 0px 2px 7px;
+  transition: all 0.2s;
                 
 }
 
--- src/Video.svelte
+++ src/Video.svelte
@@ -22,18 +22,48 @@ bind:currentTime={currentTime}
 	{(currentTime/60).toFixed(2)} /
 	{(duration / 60).toFixed(2)}
 	</p>
-<button on:click={() => paused = !paused}>
-	{paused ? "Play" : "Pause"}
+<button class="button-2" on:click={() => paused = !paused}>
+	{paused ? "12 points" : "Pause"}
 </button>
 </div>
 
 <style>
 	div {
 		position: absolute;
-		right: 0px;
+		right: 75px;
 		bottom: 200px;
 	}
 	video {
-		max-width: 600px;
+		max-width: 500px;
+	}
+
+	.button-2 {
+
+		display: inline-block;
+		outline: 0;
+		border:0;
+		cursor: pointer;
+		text-decoration: none;
+		position: relative;
+		color: #000;
+		background: rgb(238,174,202);;
+		line-height: 30px;
+		border-radius: 40px;
+		padding: 20px;
+		font-size: 30px;
+		font-weight: 600;
+		box-shadow: rgb(255, 198, 0) -2px -2px 0px 2px, rgb(246, 84, 174) 0px 0px 0px 4px, rgba(0, 0, 0, 0.05) 0px 0px 2px 7px;
+		transition: all 0.2s;
+	}
+
+	.button-2:hover {
+		background-color: yellow;
+		color: black;
+	}
+
+	button {
+		position: absolute;
+  		right: -300px;
+  		bottom: -150px;
 	}
 </style>
