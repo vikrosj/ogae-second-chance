5933c24016508d520bdcda18decec09106dc40da
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-07-31 16:41:45 +0200

    dummy 12 points file2

---
+++ voting/12-points-from.js
@@ -0,0 +1,246 @@
+import { writable } from 'svelte/store';
+
+export default twelvePointsFromArray = writable([
+  {
+    Name : "Albania",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "AL"
+},
+{
+    Name: "Armenia",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "AM"
+},
+{
+    Name: "Australia",
+    Points : 12,
+    PointsTo: "",   
+    Alpha2Code: "AU"
+},
+{
+    Name: "Austria",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "AT"
+},
+{
+    Name: "Azerbaijan",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "AZ"
+},
+{
+    Name: "Belgium",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "BE"
+},
+{
+    Name: "Bulgaria",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "BG"
+},
+{
+    Name: "Croatia",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "HR"
+},
+{
+    Name: "Cyprus",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "CY"
+},
+{
+    Name: "Czech Republic",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "CZ"
+},
+{
+    Name: "Denmark",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "DK"
+},
+{
+    Name: "Estonia",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "EE"
+},
+{
+    Name: "Finland",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "FI"
+},
+{
+    Name: "France",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "FR"
+},
+{
+    Name: "Germany",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "DE"
+},
+{
+    Name: "Georgia",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "GE"
+},
+{
+    Name: "Greece",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "GR"
+},
+{
+    Name: "Iceland",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "IS"
+},
+{
+    Name: "Ireland",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "IE"
+},
+{
+    Name: "Israel",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "IL"
+},
+{
+    Name: "Italy",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "IT"
+},
+{
+    Name: "Latvia",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "LV"
+},
+{
+    Name: "Lithuania",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "LT"
+},
+{
+    Name: "Malta",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "MT"
+},
+{
+    Name: "Moldova",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "MD"
+},
+{
+    Name: "Montenegro",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "ME"
+},
+{
+    Name: "The Netherlands",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "NL"
+},
+{
+    Name: "North Macedonia",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "MK"
+},
+{
+    Name: "Norway",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "NO"
+},
+{
+    Name: "Poland",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "PL"
+},
+{
+    Name: "Portugal",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "PT"
+},
+{
+    Name: "Romania",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "RO"
+},
+{
+    Name: "San Marino",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "SM"
+},
+{
+    Name: "Serbia",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "RS"
+},
+{
+    Name: "Slovenia",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "SI"
+},
+{
+    Name: "Spain",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "ES"
+},
+{
+    Name: "Sweden",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "SE"
+},
+{
+    Name: "Switzerland",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "CH"
+},
+{
+    Name: "Ukraine",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "UA"
+},
+{
+    Name: "United Kingdom",
+    Points : 12,
+    PointsTo: "",
+    Alpha2Code: "GB"
+}
+]
+
+  );
