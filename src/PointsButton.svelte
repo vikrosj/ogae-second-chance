62a2454b7004e58c8eca7750405d4f7b001196af
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-09-10 14:30:04 +0200

    have to fix writeable variables

--- src/PointsButton.svelte
+++ src/PointsButton.svelte
@@ -5,14 +5,15 @@
 	import { get } from 'svelte/store';
   import { andThePointsGoTo } from "../utils/pointsHandler";
   import compare from "../utils/compare";
-  import countryFlagEmoji from "country-flag-emoji";
+  import { fromCountry, visible, alpha2Code } from "./variables";
+
+  const localFromCountry = $fromCountry;
+  const localAlpha2Code = $alpha2Code;
+  const localVisible = $visible;
 
   const pointFrom = get(pointFromArray);
   
   let participantsStore = [];
-  let fromCountry = "";
-  let visible = false;
-  let alpha2Code = "GB";
 
   participantsArray.subscribe((data) => {
     participantsStore = data;
@@ -29,9 +30,9 @@
       setTimeout(() => {
       andThePointsGoTo(country.Points);
 
-      fromCountry = country.Name;
-      alpha2Code = country.Alpha2Code;
-      visible = true;
+      localFromCountry.set(country.Name);
+      localAlpha2Code.set(country.Alpha2Code);
+      LocalVisible.set(true);
 
       sortUpdate();
     }, i*1000);
@@ -43,35 +44,8 @@
     <button class="button-2" on:click={() => onClick()}>1-10 points</button>
 </div>
 
-<div class="points-text-1">
-  {#if visible}
-  <p>Points from: </p>
-  {/if}
-</div>
-
-<div class="points-text-2">
-  {#if visible}
-  <p>{fromCountry} {countryFlagEmoji.get(alpha2Code).emoji}</p>
-  {/if}
-</div>
 
 <style>
-
-.points-text-1 {
-  position: absolute;
-  right: 950px;
-  bottom: 50px;
-  font-size: 30px;
-  font-weight: 600;
-}
-
-.points-text-2 {
-  position: absolute;
-  right: 660px;
-  bottom: 50px;
-  font-size: 30px;
-  font-weight: 600;
-} 
 
 .points-button {
   position: absolute;
