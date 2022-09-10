b5d6cb2faa44a40d54637231c1429816c13eeec7
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-09-10 13:16:56 +0200

    correct export

--- voting/12-points-from.js
+++ voting/12-points-from.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
-export default twelvePointsFromArray = writable([
+export const twelvePointsFromArray = writable([
   {
     Name : "Albania",
     Points : 12,
