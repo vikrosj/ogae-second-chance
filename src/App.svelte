9cd6a394dada5137536255051ba7b74c013ec021
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-09-10 14:30:31 +0200

    move text rendering

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,14 @@
   import Table from "./Table.svelte";
   import PointsButton from "./PointsButton.svelte";
   import Video from "./Video.svelte";
-  
+  import countryFlagEmoji from "country-flag-emoji";
+  import { fromCountry, visible, alpha2Code } from "./variables" ;
+
+  const localFromCountry = $fromCountry;
+  const localAlpha2Code = $alpha2Code;
+  const localVisible = $visible;
+
+  console.log(localAlpha2Code,localFromCountry,localVisible)
 </script>
 
 <Navbar />
@@ -11,6 +18,17 @@
 <PointsButton/>
 <Video/>
 
+<div class="points-text-1">
+  {#if localVisible}
+  <p>Points from: </p>
+  {/if}
+</div>
+
+<div class="points-text-2">
+  {#if localVisible}
+  <p>{localFromCountry} {countryFlagEmoji.get(localAlpha2Code).emoji}</p>
+  {/if}
+</div>
 
 
 
@@ -20,5 +38,21 @@
     background: radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%); 
     background-size: auto;
   }
+
+  .points-text-1 {
+  position: absolute;
+  right: 950px;
+  bottom: 50px;
+  font-size: 30px;
+  font-weight: 600;
+}
+
+.points-text-2 {
+  position: absolute;
+  right: 660px;
+  bottom: 50px;
+  font-size: 30px;
+  font-weight: 600;
+} 
               
 </style>
