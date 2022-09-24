d808dd21125eb89b1485d6baa901588cd99991ca
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-09-24 10:27:25 +0200

    guest jury emoji

--- src/variables.js
+++ src/variables.js
@@ -4,5 +4,6 @@ import { writable } from 'svelte/store';
 export const fromCountry = writable("");
 export const visible = writable(false);
 export const alpha2Code = writable("GB");
+export const guestEmoji = "✨";
 
 
