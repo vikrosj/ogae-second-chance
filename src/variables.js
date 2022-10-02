a4edb54516391b5ccc223d84bf586dffc4fd7b2a
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-10-02 21:16:13 +0200

    moved variables, rm movies

--- src/variables.js
+++
@@ -1,9 +0,0 @@
-
-import { writable } from 'svelte/store';
-
-export const fromCountry = writable("");
-export const visible = writable(false);
-export const alpha2Code = writable("GB");
-export const guestEmoji = "✨";
-
-
