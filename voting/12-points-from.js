329af3cd8f8b8c4c014cacd63807ca1532bc6d87
vikrosj <31435660+vikrosj@users.noreply.github.com>
2022-10-13 15:48:10 +0200

    correct votes

--- voting/12-points-from.js
+++ voting/12-points-from.js
@@ -1,246 +1,31 @@
 import { writable } from 'svelte/store';
 
 export const twelvePointsFromArray = writable([
-  {
-    Name : "Albania",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "AL"
-},
-{
-    Name: "Armenia",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "AM"
-},
-{
-    Name: "Australia",
-    Points : 12,
-    PointsTo: "",   
-    Alpha2Code: "AU"
-},
-{
-    Name: "Austria",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "AT"
-},
-{
-    Name: "Azerbaijan",
-    Points : 12,
-    PointsTo: "United Kingdom",
-    Alpha2Code: "AZ"
-},
-{
-    Name: "Belgium",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "BE"
-},
-{
-    Name: "Bulgaria",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "BG"
-},
-{
-    Name: "Croatia",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "HR"
-},
-{
-    Name: "Cyprus",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "CY"
-},
-{
-    Name: "Czech Republic",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "CZ"
-},
-{
-    Name: "Denmark",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "DK"
-},
-{
-    Name: "Estonia",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "EE"
-},
-{
-    Name: "Finland",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "FI"
-},
-{
-    Name: "France",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "FR"
-},
-{
-    Name: "Germany",
-    Points : 12,
-    PointsTo: "United Kingdom",
-    Alpha2Code: "DE"
-},
-{
-    Name: "Georgia",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "GE"
-},
-{
-    Name: "Greece",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "GR"
-},
-{
-    Name: "Iceland",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "IS"
-},
-{
-    Name: "Ireland",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "IE"
-},
-{
-    Name: "Israel",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "IL"
-},
-{
-    Name: "Italy",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "IT"
-},
-{
-    Name: "Latvia",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "LV"
-},
-{
-    Name: "Lithuania",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "LT"
-},
-{
-    Name: "Malta",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "MT"
-},
-{
-    Name: "Moldova",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "MD"
-},
-{
-    Name: "Montenegro",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "ME"
-},
-{
-    Name: "The Netherlands",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "NL"
-},
-{
-    Name: "North Macedonia",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "MK"
-},
-{
-    Name: "Norway",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "NO"
-},
-{
-    Name: "Poland",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "PL"
-},
-{
-    Name: "Portugal",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "PT"
-},
-{
-    Name: "Romania",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "RO"
-},
-{
-    Name: "San Marino",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "SM"
-},
-{
-    Name: "Serbia",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "RS"
-},
-{
-    Name: "Slovenia",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "SI"
-},
-{
-    Name: "Spain",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "ES"
-},
-{
-    Name: "Sweden",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "SE"
-},
-{
-    Name: "Switzerland",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "CH"
-},
-{
-    Name: "Ukraine",
-    Points : 12,
-    PointsTo: "United Kingdom",
-    Alpha2Code: "UA"
-},
-{
-    Name: "United Kingdom",
-    Points : 12,
-    PointsTo: "",
-    Alpha2Code: "GB"
-}
-]
-
-  );
+{'Name': 'Ukraine', 'PointsTo': {'Poland': 12.0}, 'Alpha2Code': 'UA', 'Video': '01-Ukraine.MOV'},
+{'Name': 'Ireland', 'PointsTo': {'France': 12.0}, 'Alpha2Code': 'IE', 'Video': '02-Ireland.MOV'},
+{'Name': 'Albania', 'PointsTo': {'Finland': 12.0}, 'Alpha2Code': 'AL', 'Video': '03-Albania.MOV'},
+{'Name': 'Germany', 'PointsTo': {'Finland': 12.0}, 'Alpha2Code': 'DE', 'Video': '06-Germany.mp4'},
+{'Name': 'Sweden', 'PointsTo': {'Estonia': 12.0}, 'Alpha2Code': 'SE', 'Video': '07-Sweden.MOV'},
+{'Name': 'Australia', 'PointsTo': {'Finland': 12.0}, 'Alpha2Code': 'AU', 'Video': '08-Australia.MOV'},
+{'Name': 'ROW', 'PointsTo': {'Finland': 12.0}, 'Alpha2Code': '', 'Video': '09-ROW.mp4'},
+{'Name': 'Croatia', 'PointsTo': {'Finland': 12.0}, 'Alpha2Code': 'HR', 'Video': '10-Croatia.mp4'},
+{'Name': 'Italy', 'PointsTo': {'France': 12.0}, 'Alpha2Code': 'IT', 'Video': '11-Italy.mp4'},
+{'Name': 'Israel', 'PointsTo': {'France': 12.0}, 'Alpha2Code': 'IL', 'Video': '12-Israel.wmv'},
+{'Name': 'Norway', 'PointsTo': {'Finland': 12.0}, 'Alpha2Code': 'NO', 'Video': '13-Norway.MOV'},
+{'Name': 'Estonia', 'PointsTo': {'Finland': 12.0}, 'Alpha2Code': 'EE', 'Video':  '15-Estonia.mp4'},
+{'Name': 'Finland', 'PointsTo': {'France': 12.0}, 'Alpha2Code': 'FI', 'Video': '16-Finland.mp4'},
+{'Name': 'France', 'PointsTo': {'Spain': 12.0}, 'Alpha2Code': 'FR', 'Video': '17-France.mov'},
+{'Name': 'Iceland', 'PointsTo': {'Finland': 12.0}, 'Alpha2Code': 'IS', 'Video': '20-Iceland.mp4'},
+{'Name': 'Malta', 'PointsTo': {'Sweden': 12.0}, 'Alpha2Code': 'MT', 'Video': '22-Malta.MOV'},
+{'Name': 'Latvia', 'PointsTo': {'Estonia': 12.0}, 'Alpha2Code': 'LV', 'Video': '25-Latvia.mp4'},
+{'Name': 'Lithuania', 'PointsTo': {'Ukraine': 12.0}, 'Alpha2Code': 'LT', 'Video': '26-Lithuania.mp4'},
+{'Name': 'North-Macedonia', 'PointsTo': {'Serbia': 12.0}, 'Alpha2Code': 'MK', 'Video': '27-North-Macedonia.mp4'},
+{'Name': 'Poland', 'PointsTo': {'Spain': 12.0}, 'Alpha2Code': 'PL', 'Video': '28-Poland.MOV'},
+{'Name': 'Portugal', 'PointsTo': {'Spain': 12.0}, 'Alpha2Code': 'PT', 'Video': '29-Portugal.mp4'},
+{'Name': 'Serbia', 'PointsTo': {'Albania': 12.0}, 'Alpha2Code': 'RS', 'Video':'31-Serbia.mp4'},
+{'Name': 'Slovenia', 'PointsTo': {'Croatia': 12.0}, 'Alpha2Code': 'SI', 'Video': '32-Slovenia.mp4'},
+{'Name': 'Czech Republic', 'PointsTo': {'Sweden': 12.0}, 'Alpha2Code': 'CZ', 'Video': '37-Czech Republic.mp4'},
+{'Name': 'Denmark', 'PointsTo': {'Estonia': 12.0}, 'Alpha2Code': 'DK', 'Video': '38-Denmark.mp4'},
+{'Name': 'Spain', 'PointsTo': {'Sweden': 12.0}, 'Alpha2Code': 'ES', 'Video': '40-Spain.mp4'},
+{'Name': 'Romania', 'PointsTo': {'Sweden': 12.0}, 'Alpha2Code': 'RO', 'Video': '41-Romania.mp4'},
+]);
